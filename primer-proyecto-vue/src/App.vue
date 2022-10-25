ddfbd590622b44693e02120116cef9d7d32ed1b5
practica de git <jpromerog.usm@gmail.com>
2022-10-25 19:00:00 -0300

    Practica-Vuejs

---
+++ primer-proyecto-vue/src/App.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { ref } from "vue";
+
+const name = "Vue 3";
+
+const counter = ref(0);
+
+const increment = () => counter.value++;
+
+const decrement = () => counter.value--;
+
+const reset = () => counter.value = 0;
+
+</script>
+
+<template>
+    <h1>Hola {{ name }}!</h1>
+    <h2 :class="counter >= 0 ? 'positive' : 'negative'">
+        {{ counter }}
+    </h2>
+    <button @click="increment">Incremet</button>
+    <button @click="decrement">Decrement</button>
+    <button @click="reset">Reset</button>
+</template>
+
+<style>
+.negative {
+    color: red;
+}
+
+.positive {
+    color: green;
+}
+</style>
+
+
+
+
+
+
+
+
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style> -->
